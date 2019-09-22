b11f3808e5460145e3ef0ddffdb1176fd0cbea4d
Balamurugan R <bala@duta.in>
2019-09-22 23:11:58 +0530

    Successfully scrapped the featured album info

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
@@ -25,6 +26,19 @@ func main() {
 	//detailsPageCollector := mainPageCollector.Clone()
 	mainPageCollector.OnHTML("#featured_albums div.col-xs-6.col-sm-6.col-md-3", func(div *colly.HTMLElement) {
 		log.Println("Found a album ", div.ChildAttr("h5 a", "title"))
+		album := Album{
+			Name:          strings.TrimSpace(strings.Split(div.ChildAttr("h5 a", "title"), " - ")[0]),
+			URL:           div.Request.AbsoluteURL(div.ChildAttr("h5 a", "href")),
+			MusicDirector: strings.TrimSpace(div.ChildAttr("div.small a", "title")),
+			Starring:      strings.Split(div.ChildAttr("div span", "title"), ", "),
+		}
+		albums = append(albums, album)
+	})
+	mainPageCollector.OnHTML(`#FeatPaginate a[aria-label="Next"]`, func(a *colly.HTMLElement) {
+		url := a.Request.AbsoluteURL(a.Attr("href"))
+		log.Println("Found a next page", url)
+		mainPageCollector.Visit(url)
+
 	})
 	mainPageCollector.Visit("https://www.starmusiq.fun/home/")
 	enc := json.NewEncoder(os.Stdout)
